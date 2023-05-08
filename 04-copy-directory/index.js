113c35b393bed4a221c28c6afe411e7e4b833ec9
Yulbee <koko_joko@rambler.ru>
2023-05-08 23:52:04 +0300

    feat: complete 04-copy-directory task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+
+function copyDir () {
+    fs.mkdir(path.join(__dirname, 'files-copy'), {recursive:true}, error => {
+        if (error) return console.log(error.message);
+    });
+    fs.readdir(path.join(__dirname, 'files'), {withFileTypes:true}, (error, files) => {
+        if (error) return console.log(error.message);
+        files.forEach(dirent => {
+            fs.copyFile(path.join(__dirname, 'files', dirent.name), path.join(__dirname, 'files-copy', dirent.name), error => {
+                if (error) return console.log(error.message);
+            });
+        });
+    });
+}
+
+copyDir();
+
+    
+
 
